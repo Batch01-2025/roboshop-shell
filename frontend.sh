216b40eff417b04bcb08b77d3132fa577f479057
MannuA19 <mannmeet.ahir@gmail.com>
2025-07-16 10:00:56 +0530

    leaning functions

---
+++ frontend.sh
@@ -0,0 +1,13 @@
+!#!/bin/bash
+
+# Install Nginx
+
+dnf module disable nginx -y
+dnf module enable nginx:1.24 -y
+dnf install nginx -y
+
+# Start and Enable Nginx Service
+
+systemctl enable nginx
+systemctl start nginx
+
