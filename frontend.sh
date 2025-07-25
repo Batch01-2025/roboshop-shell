66bda1aee2cdbb18440874c6ae172b2e7eb9ddb8
MannuA19 <mannmeet.ahir@gmail.com>
2025-07-25 09:42:49 +0530

    RoboShop shell-script file

--- frontend.sh
+++ frontend.sh
@@ -2,9 +2,9 @@
 
 # Install Nginx
 
-dnf module disable nginx -y
-dnf module enable nginx:1.24 -y
-dnf install nginx -y
+sudo dnf module disable nginx -y
+sudo dnf module enable nginx:1.24 -y
+sudo dnf install nginx -y
 
 # Start and Enable Nginx Service
 
