87a49c35fa33ba2825c633aa53d282507007f706
MannuA19 <mannmeet.ahir@gmail.com>
2025-07-25 10:13:36 +0530

    RoboShop shell-script file

--- frontend.sh
+++ frontend.sh
@@ -2,43 +2,43 @@
 
 echo "Disable Nginx Module"
 sudo dnf module disable nginx -y
-status_echo $?
+status_print $?
 
 echo "Enable Nginx Module"
 sudo dnf module enable nginx:1.24 -y
-status_echo $?
+status_print $?
 
 echo "Install Nginx"
 sudo dnf install nginx -y
-status_echo $?
+status_print $?
 
 echo "Start and Enable Nginx Service"
 sudo systemctl enable nginx
-status_echo $?
+status_print $?
 
 echo "Copy the nginx config file"
 sudo cp nginx.conf /etc/nginx/nginx.conf
-status_echo $?
+status_print $?
 
 echo "Start Nginx Service"
 sudo systemctl start nginx
-status_echo $?
+status_print $?
 
 echo "Remove Nginx the default content"
 rm -rf /usr/share/nginx/html/*
-status_echo $?
+status_print $?
 
 echo "Download the frontend content"
 sudo curl -o /tmp/frontend.zip https://roboshopartifacts.s3.amazonaws.com/frontend-v3.zip
-status_echo $?
+status_print $?
 
 echo "Extract the frontend content"
 sudo unzip /tmp/frontend.zip -d /usr/share/nginx/html
-status_echo $?
+status_print $?
 
 echo "Restart nginx"
 sudo systemctl restart nginx
-status_echo $?
+status_print $?
 
 
 
