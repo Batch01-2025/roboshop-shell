15828505a0149dd040deb6889ac64c7ec935afb6
MannuA19 <mannmeet.ahir@gmail.com>
2025-07-31 20:57:30 +0530

    RoboShop shell-script file

---
+++ catalogue.sh
@@ -0,0 +1,69 @@
+#! /bin/bash
+
+echo "Disable Nodejs Module"
+sudo dnf module disable nodejs -y
+echo "Exit status: $?"
+
+echo "Enable Nodejs Module"
+sudo dnf module enable nodejs:20 -y
+echo "Exit status: $?"
+
+echo "Install Nodejs"
+sudo dnf install nodejs -y
+echo "Exit status: $?"
+
+echo "Adding the user"
+sudo useradd roboshop
+echo "Exit status: $?"
+
+echo " creating Directory app"
+sudo mkdir /app
+echo "Exit status: $?"
+
+echo "Download the application code"
+sudo curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
+sudo cd /app
+sudo unzip /tmp/catalogue.zip
+echo "Exit status: $?"
+
+echo "Installing npm dependencies"
+sudo cd /app
+sudo npm install
+echo "Exit status: $?"
+
+echo "Setup the catalogue service"
+sudo cp catalogue.service /etc/systemd/system/catalogue.service
+echo "Exit status: $?"
+
+echo "Load the Service"
+sudo systemctl daemon-reload
+echo "Exit status: $?"
+
+echo "Enable and Start the Catalogue Service"
+sudo systemctl enable catalogue
+sudo systemctl start catalogue
+echo "Exit status: $?"
+
+echo "Copy MongoDB Client Repo"
+sudo cp mongodb.repo /etc/yum.repos.d/mongo.repo
+echo "Exit status: $?"
+
+echo "Install MongoDB Client"
+sudo dnf install mongodb-mongosh -y
+echo "Exit status: $?"
+
+echo "Load Master Data"
+sudo mongosh --host 44.203.90.105 </app/db/master-data.js
+echo "Exit status: $?"
+
+
+
+
+
+
+
+
+
+
+
+
