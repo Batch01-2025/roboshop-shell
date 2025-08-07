d86a21856116ecd84666611fa4529036766f42fa
MannuA19 <mannmeet.ahir@gmail.com>
2025-08-07 20:52:43 +0530

    RoboShop shell-script file

--- dispatch.sh
+++ dispatch.sh
@@ -1,19 +1,21 @@
 #! /bin/bash
 source ./common.sh
 component=dispatch
+LOG_FILE=/tmp/roboshop.log
+rm -f /tmp/roboshop.log
 
 echo "Install Go Lang"
-sudo dnf install golang -y
+sudo dnf install golang -y &>>$LOG_FILE
 echo "Exit Status: $?"
 
-APP_SETUP
+APP_SETUP &>>$LOG_FILE
 
 echo "Build the Dispatch Service"
 cd /app
-sudo go mod init dispatch
-sudo go get
-sudo go build
-echo "Exit Status: $?"
+sudo go mod init dispatch &>>$LOG_FILE
+sudo go get &>>$LOG_FILE
+sudo go build &>>$LOG_FILE
+echo "Exit Status: $?" &>>$LOG_FILE
 
-SYSTEMD_SETUP
+SYSTEMD_SETUP &>>$LOG_FILE
 
